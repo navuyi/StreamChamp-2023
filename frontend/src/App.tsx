839062c9de95264b1647d278841db0e3f042c082
navuyi <figlusrafal@gmail.com>
2023-06-25 23:03:35 +0200

    testing imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import {SignInRequestBody} from "@backend/types/authentication.type"
+
 function App() {
   const [count, setCount] = useState(0)
+  
+  const item : SignInRequestBody = {
+    email: "sd",
+    password: "sds"
+  }
+  console.log(item)
 
   return (
     <>
