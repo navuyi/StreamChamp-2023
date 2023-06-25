7922c44486944e8308d5ea80960644d00477b435
navuyi <figlusrafal@gmail.com>
2023-06-25 18:51:28 +0200

    streamer controllers

---
+++ backend/src/controllers/streamer.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import { CreateStreamerRequestBody } from "../types/streamer.type";
+import { validationResult } from "express-validator";
+import { Streamer } from "../database/models/Streamer";
+
+const postStreamer = async (req:Request, res:Response, next:NextFunction) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        res.status(400).json(errors)
+        return
+    }
+    // Add streamer to database
+    const body = req.body as CreateStreamerRequestBody
+    await Streamer.create(body)
+    console.log(body)
+    res.status(201).json({
+        msg: "Streamer created"
+    })
+}
+
+const getStreamer = async (req:Request, res:Response, next:NextFunction) => {
+    const data = await Streamer.findAll()
+    res.json(data)
+}
+
+export {
+    postStreamer,
+    getStreamer
+}
