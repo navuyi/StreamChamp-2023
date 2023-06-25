0e8514f3cc18606815a4f8bbc1ec175849674b7f
navuyi <figlusrafal@gmail.com>
2023-06-25 20:52:23 +0200

    application class - for separate testing instance

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,72 +1,54 @@
 import * as express from "express"
 import * as cors from "cors"
+import * as process from "process"
 import urlRouter from "./routes/url"
+import streamerRouter from "./routes/streamer"
+import { Application } from "express"
+import * as http from "http"
 import db from "./database/config"
-import { Vote } from "./database/models/Vote"
-import { Streamer } from "./database/models/Streamer"
-import { where } from "sequelize"
-import { User } from "./database/models/User"
-const app = express()
-//Streamer.belongsToMany(User, {through: UserStreamerVotes})
-//User.belongsToMany(Streamer, {through: UserStreamerVotes})
 
-db.sync({force: true}).then(async () => {
-   
-    await Streamer.create({
-        firstName: "s",
-        lastName: "sds",
-        nickname: "f",
-        platform: "f",
-        description: "d"
-    })
-    await Streamer.create({
-        firstName: "sdddd",
-        lastName: "sds",
-        nickname: "fddd",
-        platform: "f",
-        description: "d"
-    })
-    await User.create({
-        firstName: "d",
-        lastName: "d",
-        email: "d@wp.pl",
-        password: "d"
-    })
-    await User.create({
-        firstName: "d",
-        lastName: "d",
-        email: "d@wsdsdp.pl",
-        password: "d"
-    })
 
-    const vote = await Vote.create({
-        value: "upvote",
-        user_id: 1,
-        streamer_id: 1
-    })
-    const voste = await Vote.create({
-        value: "downvote",
-        user_id: 1,
-        streamer_id: 2
-    })
-    const voasdste = await Vote.create({
-        value: "downvote",
-        user_id: 2,
-        streamer_id: 2
-    })
-    const voasdsdste = await Vote.create({
-        value: "downvote",
-        user_id: 2,
-        streamer_id: 1
-    })
-})
+export class App {
+    private app: Application | null = null
+    private server: http.Server | null = null
 
-app.use(cors({
-    origin: ["TODO"]
-}))
-app.use("/yt", urlRouter)
+    public init = async () : Promise<Application> => {
+        this.app = express()
+        // Database
+        db.sync(process.env.NODE_ENV==="test" ? {force: true} : {alter: true})
+        // Cors
+        this.app.use(cors({
+            origin: ["TODO"]
+        }))
+        // Request body parser
+        this.app.use(express.json())
 
-const PORT = 8080
-app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}`)
-})
+        // Endpoints
+        this.app.use("/yt", urlRouter)
+        this.app.use("/streamers", streamerRouter)
+        // ... 
+        // ...
+
+
+        // Start http server
+        const PORT = process.env.NODE_PORT || 8080;
+        this.server = this.app.listen(PORT, () => {
+            console.log(`Listening on ${PORT}`);
+        });
+        
+        return this.app
+    }
+
+    public close = async () => {
+        this.server.close()
+    }
+}   
+
+if(!process.env.NODE_ENV){
+    const app = new App()
+    app.init()
+}
+
+
+
+
