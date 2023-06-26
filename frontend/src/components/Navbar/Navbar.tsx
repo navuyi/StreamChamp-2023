15986af453b6119b8d49c24d2576fe552b407851
navuyi <figlusrafal@gmail.com>
2023-06-27 01:09:47 +0200

    emotion keyframes

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import { styles } from "./style"
 const Navbar = () => {
     return(
         <div css={styles.container}>
+            <div css={styles.button}></div>
             <header css={styles.header}>StreamChamp</header>
         </div>
     )
--- frontend/src/components/Navbar/style.ts
+++ frontend/src/components/Navbar/style.ts
@@ -1,5 +1,17 @@
-import { css } from "@emotion/react"
+import { css, keyframes } from "@emotion/react"
 import { Colors, Fonts } from "../../config/style"
+
+import icon from "../../assets/icons/logo.png"
+
+
+const spin = keyframes({
+    "0%": {
+        rotate: "45deg"
+    },
+    "100%":{
+        rotate: "405deg"
+    }
+})
 
 export const styles = {
     container: css({
@@ -12,6 +24,23 @@ export const styles = {
     header: css({
         fontSize: "20px",
         color: Colors.white,
-        fontFamily: Fonts.PoppinsLight
+        fontFamily: Fonts.PoppinsRegular,
+        marginLeft: "2em"
+    }),
+    button: css({
+        width: 32,
+        height: 32,
+        backgroundImage: `url(${icon})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        rotate: "45deg",
+        marginLeft: "2em",
+        cursor: "pointer",
+
+        ":hover": {
+            animation: `${spin} 1s ease forwards`
+            
+        }
     })
 }
+
