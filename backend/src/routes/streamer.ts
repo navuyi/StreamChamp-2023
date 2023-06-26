3b014f4cae455874d3b35b9313bf3f79d109b0a7
navuyi <figlusrafal@gmail.com>
2023-06-26 21:17:22 +0200

    fixed colliding endpoint path

--- backend/src/routes/streamer.ts
+++ backend/src/routes/streamer.ts
@@ -5,7 +5,7 @@ import { createStreamerValidator, getStreamerWithIDValidator} from "../validator
 const router = Router()
 
 router.post("/", createStreamerValidator, streamerController.postStreamer)
-router.get("/", streamerController.getStreamer)
+router.get("/page/:page", streamerController.getStreamer)
 router.get("/:id", getStreamerWithIDValidator, streamerController.getStreamerWithID)
 
 export default router
