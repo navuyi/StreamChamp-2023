1b837482905d56b72c438f8699e0e086908ec31b
navuyi <figlusrafal@gmail.com>
2023-06-27 19:34:57 +0200

    custom TextInput component

---
+++ frontend/src/components/TextInput/TextInput.tsx
@@ -0,0 +1,27 @@
+
+import { StreamerFormTextAttributes } from "../../redux/features/streamerFormSlice"
+import style from "./style.module.scss"
+
+
+type props = {
+    label: string
+    placeholder?: string
+    value: string
+    _key: StreamerFormTextAttributes
+    textarea? : boolean
+    handleChange: (key:StreamerFormTextAttributes, value:string) => void
+}
+
+const TextInput = (props:props) => {
+    return(
+        <div className={style.inputContainer}>
+            <label className={style.label}>{props.label}</label>
+            {props.textarea === true ? 
+                <textarea className={style.input} rows={6} placeholder={props.placeholder} value={props.value} onChange={e => props.handleChange(props._key, e.currentTarget.value)}/>
+                :<input className={style.input} placeholder={props.placeholder} value={props.value} onChange={e => props.handleChange(props._key, e.currentTarget.value)}/>    
+            }
+        </div>
+    )
+}
+
+export default TextInput
