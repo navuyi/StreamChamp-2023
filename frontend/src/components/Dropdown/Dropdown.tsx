58d27a7487078cb372de480a662803b5eb843196
navuyi <figlusrafal@gmail.com>
2023-06-27 15:31:38 +0200

    navbar and landing page

--- frontend/src/components/Dropdown/Dropdown.tsx
+++ frontend/src/components/Dropdown/Dropdown.tsx
@@ -1,16 +1,24 @@
-
-
+import { useEffect, useState } from "react"
 import style from "./style.module.scss"
 
 
 const Dropdown = () => {
-    
+    const [active, setActive] = useState<boolean>(false)
+
+    useEffect(() => {
+        
+    }, [active])
+
+    const handleDropdownOpen = () => {
+        setActive(prev => !prev)
+    }
+
     return(
-        <div className={style.container}>
-            <div className={style.button}>
+        <div className={style.dropdown}>
+            <div className={style.button} onClick={handleDropdownOpen}>
 
             </div>
-            <div className={style.content} >
+            <div className={style.content} data-active={active} onMouseLeave={() => setActive(false)}>
                 <a href="#">Link 1</a>
                 <a href="#">Link 2</a>
                 <a href="#">Link 3</a>
--- frontend/src/views/Landing/Landing.tsx
+++ frontend/src/views/Landing/Landing.tsx
@@ -5,8 +5,17 @@ import style from "./style.module.scss"
 const Landing = () => {
 
   return(
-    <div className={style.container}> 
+    <div className={style.landing}> 
       <Navbar />
+      <div className={style.container}>
+        <header className={style.header}>Welcome to <span>StreamChamp</span> - a streamer spotlight application </header>
+        <span className={style.text}>
+          Lorem ipsum dolor sit amet consectetur. Sit felis convallis ante velit faucibus ut. Eros placerat pellentesque mi in proin. Tellus vel a purus ornare cursus. Dolor id pharetra neque convallis sit eu. Ligula aliquam nec at aliquam nulla ut.
+        </span>
+        <span className={style.text}>
+          Feugiat euismod purus. Lorem ipsum dolor sit amet consectetur. Bibendum ornare feugiat leo pretium. Porta tortor lectus lacus eget suspendisse eget purus cursus risus. Cras eleifend tristique tempor arcu sed natoque. Vel cras ante interdum sed neque pellentesque nisi ullamcorper neque. 
+        </span>
+      </div>
     </div>
   )
 }
