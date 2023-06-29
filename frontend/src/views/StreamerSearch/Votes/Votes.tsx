1240ab5763856142afc7e923148a206b410381b0
navuyi <figlusrafal@gmail.com>
2023-06-29 21:06:33 +0200

    hover, preventing bubbling

--- frontend/src/views/StreamerSearch/Votes/Votes.tsx
+++ frontend/src/views/StreamerSearch/Votes/Votes.tsx
@@ -2,6 +2,7 @@
 import style from "./style.module.scss"
 import {ReactComponent as UpvoteArrow} from "../../../assets/icons/upvote.svg"
 import {ReactComponent as DownvoteArrow} from "../../../assets/icons/downvote.svg"
+import { MouseEvent } from "react"
 
 export interface voteProps {
     upvotes: number
@@ -10,16 +11,22 @@ export interface voteProps {
 }
 
 const Votes = (props:voteProps) => {
+
+    const handleClick = (e:MouseEvent<SVGSVGElement>) => {
+        e.stopPropagation() 
+        console.log("Vote Arrow clicked")
+    }
+
     return(
         <div className={style.votes}>
             <div className={style.votesSubcontainer}>
                 
-                <UpvoteArrow className={style.arrow} data-active={props.vote === "upvote" ? true : false} />
+                <UpvoteArrow className={style.arrow} data-active={props.vote === "upvote" ? true : false} onClick={handleClick} />
                 <span className={style.counter}>{props.upvotes}</span>
             </div>
             <div className={style.votesSubcontainer}>
                 <span className={style.counter}>{props.downvotes}</span>
-                <DownvoteArrow className={style.arrow} data-active={props.vote === "downvote" ? true : false} />
+                <DownvoteArrow className={style.arrow} data-active={props.vote === "downvote" ? true : false} onClick={handleClick} />
             </div>
         </div>
     )
