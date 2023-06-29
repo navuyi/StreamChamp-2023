191b1ec99c8b37ae342f6dc43750729e06d3b608
navuyi <figlusrafal@gmail.com>
2023-06-29 23:34:45 +0200

    parsing vote numbers, ui tweaks

--- frontend/src/views/StreamerSearch/Votes/Votes.tsx
+++ frontend/src/views/StreamerSearch/Votes/Votes.tsx
@@ -17,15 +17,25 @@ const Votes = (props:voteProps) => {
         console.log("Vote Arrow clicked")
     }
 
+    const parseVotes = (value:number) => {
+        if (value >= 1000 && value < 1000000) {
+            return (value / 1000).toFixed(1) + "k";
+          } else if (value >= 1000000) {
+            return (value / 1000000).toFixed(1) + "m";
+          } else {
+            return value.toString();
+          }
+    }
+
     return(
         <div className={style.votes}>
             <div className={style.votesSubcontainer}>
                 
                 <UpvoteArrow className={style.arrow} data-active={props.vote === "upvote" ? true : false} onClick={handleClick} />
-                <span className={style.counter}>{props.upvotes}</span>
+                <span className={style.counter}>{parseVotes(props.upvotes)}</span>
             </div>
             <div className={style.votesSubcontainer}>
-                <span className={style.counter}>{props.downvotes}</span>
+                <span className={style.counter}>{parseVotes(props.downvotes)}</span>
                 <DownvoteArrow className={style.arrow} data-active={props.vote === "downvote" ? true : false} onClick={handleClick} />
             </div>
         </div>
