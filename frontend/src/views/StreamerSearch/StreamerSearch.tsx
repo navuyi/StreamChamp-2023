8a272914b1bff8f15d2c79769207659fddf8293b
navuyi <figlusrafal@gmail.com>
2023-06-29 22:32:07 +0200

    UI tweaks

--- frontend/src/views/StreamerSearch/StreamerSearch.tsx
+++ frontend/src/views/StreamerSearch/StreamerSearch.tsx
@@ -12,11 +12,9 @@ const StreamerSearch = () => {
                 <div className={style.wrapper}>
                     <span className={style.subHeader}>Recently added</span>
                     <div className={style.recentlyAddedContainer}>
-                        <StreamerBadge type="recent" nickname="xqcDramaHelloWorld" upvotes={12} downvotes={1} vote={"downvote"}/>
-                        <StreamerBadge type="recent" nickname="xqc" upvotes={22} downvotes={12} vote={null}/>
-                        <StreamerBadge type="recent" nickname="forsen" upvotes={52} downvotes={31} vote={"upvote"}/>
-                        <StreamerBadge type="recent" nickname="xqc" upvotes={22} downvotes={12} vote={null}/>
-                        <StreamerBadge type="recent" nickname="xqc" upvotes={22} downvotes={12} vote={null}/>
+                        <StreamerBadge type="recent" nickname="horsensen" upvotes={12} downvotes={1} vote={"downvote"}/>
+                        <StreamerBadge type="recent" nickname="xqc" upvotes={222} downvotes={12} vote={null}/>
+                        <StreamerBadge type="recent" nickname="forsen" upvotes={52} downvotes={331} vote={"upvote"}/>
                     </div>
                 </div>
                 <div className={style.wrapper}>
