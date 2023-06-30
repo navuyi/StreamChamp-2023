31271d2dbd9d4a36a7a5a63b6b03570279532720
navuyi <figlusrafal@gmail.com>
2023-06-30 12:27:40 +0200

    redirecting to signin and signup

--- frontend/src/components/Modal/SignInModal/SignInModal.tsx
+++ frontend/src/components/Modal/SignInModal/SignInModal.tsx
@@ -2,21 +2,28 @@ import style from "./style.module.scss"
 import { modalComponentProps } from "../../../redux/features/modalSlice"
 import { useAppDispatch } from "../../../redux/store"
 import { setModalVisible } from "../../../redux/features/modalSlice"
+import { useNavigate } from "react-router"
 
 const SignInModal = (props:modalComponentProps) => {
     const dispatch = useAppDispatch()
+    const navigate = useNavigate()
 
     const handleClose = () => {
         dispatch(setModalVisible(false))
     }
 
+    const handleNavigate = (dest:string) => {
+        dispatch(setModalVisible(false))
+        navigate(dest)
+    }
+    
     return(
         <div className={style.signinModal}>
             <section className={style.section}>
                 <header>{props.header}</header>
                 <text>{props.text}</text>
-                <span><span className={style.link}>Sign in</span> to have full control.</span>
-                <span>Don't have account yet? <span className={style.link}>Sign up here</span></span>
+                <span><span className={style.link} data-dest="/auth/signin" onClick={(e) => handleNavigate(e.currentTarget.dataset.dest!)}>Sign in</span> to have full control.</span>
+                <span>Don't have account yet? <span className={style.link} data-dest="/auth/signup" onClick={(e) => handleNavigate(e.currentTarget.dataset.dest!)}>Sign up here</span></span>
                 <button onClick={handleClose}>Dismiss</button>
             </section>
         </div>
