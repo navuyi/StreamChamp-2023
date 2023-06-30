47e572365c13ea969a6746005c9f0d1e9f9ba360
navuyi <figlusrafal@gmail.com>
2023-06-30 10:30:42 +0200

    dropwodn outside click closing

--- frontend/src/components/Dropdown/Dropdown.tsx
+++ frontend/src/components/Dropdown/Dropdown.tsx
@@ -1,27 +1,47 @@
 import { useEffect, useState } from "react"
 import style from "./style.module.scss"
-
+import { useNavigate } from "react-router"
+import { useRef } from "react"
 
 const Dropdown = () => {
     const [active, setActive] = useState<boolean>(false)
+    const navigate = useNavigate()
+    const dropdownRef = useRef<HTMLDivElement | null>(null)
 
     useEffect(() => {
-        
-    }, [active])
+        const handleClickOutside = (event: MouseEvent) => {
+          if (
+            dropdownRef.current &&
+            !dropdownRef.current.contains(event.target as Node)
+          ) {
+            setActive(false);
+          }
+        };
+    
+        document.addEventListener("click", handleClickOutside);
+    
+        return () => {
+          document.removeEventListener("click", handleClickOutside);
+        };
+      }, []);
+
+    const handleNavigate = (dest:string) => {
+        navigate(dest)
+    }
 
     const handleDropdownOpen = () => {
         setActive(prev => !prev)
     }
 
     return(
-        <div className={style.dropdown}>
-            <div className={style.button} onClick={handleDropdownOpen}>
-
-            </div>
+        <div className={style.dropdown} ref={dropdownRef}>
+            <div className={style.button} onClick={handleDropdownOpen}></div>
             <div className={style.content} data-active={active} onMouseLeave={() => setActive(false)}>
-                <a href="#">Link 1</a>
-                <a href="#">Link 2</a>
-                <a href="#">Link 3</a>
+                <a data-dest={"/"} onClick={(e) => handleNavigate(e.currentTarget.dataset.dest!)}>Home</a>
+                <a data-dest={"/streamer/create"} onClick={(e) => handleNavigate(e.currentTarget.dataset.dest!)}>Add streamer</a>
+                <a data-dest={"/streamer/search"} onClick={(e) => handleNavigate(e.currentTarget.dataset.dest!)}>Search streamers</a>
+                <a data-dest={"/auth/signin"} onClick={(e) => handleNavigate(e.currentTarget.dataset.dest!)}>Sign In</a>
+                <a data-dest={"/auth/signup"} onClick={(e) => handleNavigate(e.currentTarget.dataset.dest!)}>Sign Up</a>
             </div>
         </div>
     )
